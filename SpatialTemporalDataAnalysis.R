e00a5151f19fe2c413016c9b8a89aa6ed598c7bb
HaigangLiu <haigang@email.sc.edu>
2017-03-08 19:11:35 -0500

    Update SpatialTemporalDataAnalysis.R

--- SpatialTemporalDataAnalysis.R
+++ SpatialTemporalDataAnalysis.R
@@ -1,7 +1,10 @@
-
-library(spTimer); 
-library(coda); 
+library(spTimer)
+library(coda)
 library(graphics)
+
+# this program is designed to implement the AR model from spTimer package
+# two scenario: one year (2015) and five year (2011 - 2015)
+# change chunk size to 60 before doing the five year case
 
 setwd("/Users/haigangliu/Dropbox/DataRepository/")
 Year2015 = read.csv("Year2015RangedTempTrendRemoved.csv")
@@ -10,8 +13,10 @@ FiveYears = read.csv("FiveYearRangedTempTrendRemoved.csv")
 chunk_size = 12
 
 training_test_splitter = function(data, chunk_size){
-  # chunk size = 12 if its one year, and 24 if two years, so on and so forth
-  # will return the training set and test set in a list
+  
+  # function will return the training set and test set in a list
+  # NOTE: we can only shuffle locations but cannot shuffle months!!!
+  
   num_of_locs = dim(data)[1]/chunk_size
   randomized_indices = rep(sample(num_of_locs, size = num_of_locs), each = chunk_size )
   data["randomized_indices"] = randomized_indices 
@@ -28,41 +33,37 @@ dataCleaner  <- function(data_set) {
   
   # just to rename all the data to be compatible to legacy code 
   data_set = data_set[c("YEAR", "MONTH","LATITUDE","LONGITUDE","ELEVATION", "PRCP", "STATION_NAME", "RANGE_HIGH", "RANGE_LOW", "RANGE_OVERALL","SST", "RESIDUAL", "cos", "sin")]
-  
   names(data_set) = c("Year", "Month","Latitude","Longitude","ELEVATION", "PRCP","STATION_NAME","RANGE_HIGH", "RANGE_LOW", "RANGE_OVERALL","SST", "RESIDUAL", "cos", "sin")
 
-  # try(data_set$Latitude <- as.numeric(levels(data_set$Latitude)[data_set$Latitude]), silent = T)
-  # try(data_set$Longitude <- as.numeric(levels(data_set$Longitude)[data_set$Longitude]),silent = T)
-  # try(data_set$ELEVATION <- as.numeric(levels(data_set$ELEVATION)[data_set$ELEVATION]),silent = T) 
-  # try(data_set$RANGE_OVERALL<- as.numeric(levels(data_set$RANGE_OVERALL)[data_set$RANGE_OVERALL]),silent = T) 
-  # try(data_set$RANGE_HIGH<- as.numeric(levels(data_set$RANGE_HIGH)[data_set$RANGE_HIGH]),silent = T) 
-  # try(data_set$RANGE_LOW<- as.numeric(levels(data_set$LOW)[data_set$RANGE_LOW]),silent = T) 
-  
-  print(paste("finished cleaning, the missing record is number is", sum(is.na(data_set))))
   return(data_set)
 }
 
 
 theResidualRetriever = function(model_ouptut, new_data){
+  
+  # get the predictiion residuals from test set (new_data)
+  
   prediction =  predict(model_ouptut, newdata = new_data, newcoords = ~ Longitude + Latitude)
   monthly_pred = apply(prediction$pred.samples, 1, median)
+  
   matrix_pred =  matrix(data = monthly_pred, nrow = dim(new_data)[1]/chunk_size , ncol = chunk_size, byrow = FALSE)
-  recovered_data = 10^(matrix_pred) - (overhead + amount_to_make_positive)
-  
-  print(dim(recovered_data))
-  
+  recovered_data = 10^(matrix_pred) - (overhead + amount_to_make_positive) # back-transformed 
+ 
   return(recovered_data)
 }
 
 
 theOriginalDataHandler = function(new_data){
+  # reshape the observed data to be comparable to the prediction
   observed = new_data$RESIDUAL
   reshaped  = matrix(data = observed, nrow = dim(new_data)[1]/chunk_size, ncol = chunk_size, byrow = FALSE)
   return(reshaped)
 }
 
 thePlotter = function(the_pred, the_original){
-  par(mfrow = c(5,6))
+  
+  # plot the prediction v.s. observed for 12/60 months   
+  par(mfrow = c(3,4))
   for (i in 1:chunk_size){
     plot(the_pred[,i], 2*the_original[,i], main = paste("month", i), ylab = "observed", xlab ="predicted")
     abline(0,1)
@@ -70,14 +71,17 @@ thePlotter = function(the_pred, the_original){
 }
 
 pipeline = function(data, logbase = 10 ){
-  #log transformation
-  type_corrected_data = dataCleaner(data)
   
-  amount_to_make_positive = abs(min(type_corrected_data ["RESIDUAL"]))
+  # call all previous functions to do data munging
+  # after that the AR model from spTimer is called
+  # followed by diagnosis plots
+  
+  name_corrected_data = dataCleaner(data)
+  
+  amount_to_make_positive = abs(min(name_corrected_data ["RESIDUAL"]))
   overhead = 0.01
   type_corrected_data["LOG"] = log(amount_to_make_positive + overhead + type_corrected_data["RESIDUAL"], base = logbase)
   
-  #cleaning the type
   
   #split the data into training and test
   splitted = training_test_splitter(type_corrected_data, chunk_size = chunk_size )
@@ -101,6 +105,7 @@ pipeline = function(data, logbase = 10 ){
 }
 
 chunksize = 12
-kk = pipeline(Year2015)
+output_of_2015 = pipeline(Year2015)
 
-
+chunksize = 60
+output_of_2015 = pipeline(FiveYears)
