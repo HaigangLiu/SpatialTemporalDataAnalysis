ec2345a5c6cdb2aabaf05aa496bc6c8315d244a7
HaigangLiu <haigang@email.sc.edu>
2017-03-19 14:23:28 -0400

    Bug fix
    Corrected the order of latitude and longitude. Note that the haversine function assumes latitude comes first and longitude second

--- MissingDataHandler.py
+++ MissingDataHandler.py
@@ -1,52 +1,44 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 from haversine import haversine
 from functools import partial 
 from scipy.stats import norm
 
-# this program is designed to handle missing data in covariates
-# the idea is to get the average of its neighbors, which are not equally important.
-# the importance is determined by distances, the kernel function is normal
-# there is no "shutdown" threshold like we do in SST b/c all neighbors in this case are pretty close.
-
 class missingDataHandler(object):
-    
     def __init__(self, data, feature):
-               
+        
         self.data = data
-        self.feature =feature
+        self.feature = feature
         
-        if self.feature.startswith("RANGE"):
-            self.known_data = data[data[feature] > 0]
-            self.missing_data = data[data[feature] <= 0]
+        if feature.startswith("RANGE"):
+            self.known_data = self.data[self.data[feature] > 0]
+            self.missing_data = self.data[self.data[feature] <= 0]
+            
         else:
-            self.known_data = data[data[feature] >= 0]
-            self.missing_data = data[data[feature] < 0]
-        
-        self.known_data["LATITUDE"] = pd.to_numeric(self.known_data["LATITUDE"])
-        self.known_data["LONGITUDE"] = pd.to_numeric(self.known_data["LONGITUDE"])
+            self.known_data = self.data[self.data[feature] >= 0]
+            self.missing_data = self.data[self.data[feature] < 0]
+            
+        #extract the index of known data for better performance
+        self.month_index = self.known_data.set_index(["MONTH"]).index
+        self.year_index = self.known_data.set_index(["YEAR"]).index
+            
+    def complete_the_missing_feature(self,rec):
+        lon = rec.LONGITUDE
+        lat = rec.LATITUDE
+        partial_haversine = partial(haversine, (lat, lon), miles = True)
 
-    def complete_the_missing_feature(self, rec):
-        lon = float(rec.LONGITUDE); lat = float(rec.LATITUDE)
-        partial_haversine = partial(haversine, (lon, lat), miles = True)
-        
-        known_info = self.known_data[(self.known_data.MONTH == rec.MONTH) & (self.known_data.YEAR == rec.YEAR)]
+        known_info = self.known_data[(self.month_index == rec.MONTH) & (self.year_index == rec.YEAR)]
         distance = known_info[[ "LATITUDE","LONGITUDE"]].apply(partial_haversine, axis = 1)
-        
+
         scale_param = 0.25*(np.max(distance) - np.min(distance))
-        pdf1 = norm.pdf(distance, loc = np.mean(distance), scale = scale_param)       
-        filled_feature = sum((pdf1/sum(pdf1))*known_info[self.feature])
-        
+        pdf1 = norm.pdf(distance, loc = 0, scale = scale_param)       
+        filled_feature = np.dot((pdf1/np.sum(pdf1)), known_info[self.feature])
+
         return filled_feature
-    
-    # its called df organizer b/c it will return an organized df with all missing data filled.
+
     def data_frame_organizer(self):
-        del self.missing_data[self.feature]
-        k = self.missing_data.apply(self.complete_the_missing_feature, axis = 1)
-        self.missing_data[self.feature] = k
+        self.missing_data[self.feature] = self.missing_data.apply(self.complete_the_missing_feature, axis = 1)
         completed_version = self.known_data.append(self.missing_data)
         output = completed_version.sort_values(["STATION_NAME", "YEAR","MONTH"])
-        return output
-        
+        return output.iloc[:,1:]
